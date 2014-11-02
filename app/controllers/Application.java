1ba90767cd00e07b2974eb5578dca37bf5204ba5
Arshdeep Sabharwal <arshdeepsabharwal14@mittymonarch.com>
2014-11-02 17:28:03 -0600

    Bare bones play server setup.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,12 +3,30 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+import java.io.*;
+import java.util.*;
+import model.*;
 import views.html.*;
 
 public class Application extends Controller {
+	private static final List<Source> sources = Source.ReadSourcesFromFile("others/sources.txt");
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
 
+
+    public static Result search() {
+    	return ok(search.render());
+    }
+
+    public static Result results(String query) throws IOException {
+		
+		System.out.println("Searching for \"" + query + "\"");
+		Map<String, List<String>> lists = Search.search(query, sources);
+
+		Search.printLists(lists);
+
+		return ok(results.render(query, lists));
+    }
 }
